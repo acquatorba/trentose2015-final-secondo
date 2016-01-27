bd257e0f41cc2ef4c0646712e28c99b50cf80542
Giulio Maria Facin <giulio.maria@gmail.com>
2016-01-27 15:38:47 +0100

    firstsprint

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
 var FlirtrModel = {  
-  
+   
+   init: function(){
+       var score=0;
+   },
+    
    /* It sets the phrase to analyse */
    analyse : function (phrase){
+        var res = phrase.split(" ");
+        score=0;
+        for(var i=0;i<res.length;i++){
+            for(var j=0;j<patterns.length;j++){
+                if(res[i].word==patterns[j].word){
+                    score=score+patterns[j].score;
+                }
+            }
+        }
    },
   
    /* Returns the flirting score (number) of the phrase.
@@ -10,7 +23,41 @@ var FlirtrModel = {
     * patterns (unless the pattern neutraliser is found to 
     * the right of the matching word)
     */
-   getScore : function () {
+   getScore: function () {
+       return score;
    }
          
 };
+
+var octopus ={
+    init: function(){
+        view.init();
+        FlirtrModel.init();
+    },
+    controllatext: function(testo){
+        FlirtrModel.analyse(testo);
+    },
+    getscore: function(){
+        return FlirtrModel.getScore();
+    }
+};
+
+var view ={
+    init: function(){
+        view.startbutton();
+    },
+    startbutton: function(){
+        $(".check").on("click", function (event) {
+                  var text=$("#phrase").val();
+                  octopus.controllatext(text);
+                  var punti=octopus.getscore();
+                  $(".answer").append(punti);
+              })
+    }
+};
+
+
+
+octopus.init();
+
+
